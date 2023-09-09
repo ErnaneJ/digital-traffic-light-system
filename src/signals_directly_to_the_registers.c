3a791ea0422e7b28a97c0078f37a0a265d044854
Thiago Lopes do Nascimento <thiagonasmento20@gmail.com>
2023-09-09 18:45:06 -0300

    edit code semaphore-registers

--- src/signals_directly_to_the_registers.c
+++ src/signals_directly_to_the_registers.c
@@ -6,27 +6,29 @@
 // We are using ports 4, 5 and 6 on the Arduino to output the red, 
 // yellow and green LED signals respectively
 
-// These ports correspond to the D register group on the ATmega328p microcontroller
+// These ports correspond to the G, E and H registers group on the ATmega2560 microcontroller
 
 #include <util/delay.h>
 
 int main(void)
 {              // 0GYR 0000
-  DDRD = 0x70; // 0111 0000 => setting only the necessary ports as output
+  DDRG = 0x20; // 0010 0000 => setting only the necessary ports as output (pin 4 board Arduino)
+  DDRE = 0x8;  // 0000 1000 => setting only the necessary ports as output (pin 5 board Arduino)
+  DDRH = 0x8;  // 0000 1000 => setting only the necessary ports as output (pin 6 board Arduino)
   while(1)
   {
     // closed sign
-    PORTD = 0x10; // 0001 0000 => turning on the red LED
+    PORTG = 0x20; // 0010 0000 => turning on the red LED
     _delay_ms(5000); // 5 seconds
-    
+    PORTG = 0x00;
     // attention sign
-    PORTD = 0x20; // 0010 0000 => turning on the yellow LED
+    PORTE = 0x8; // 0000 1000 => turning on the yellow LED
     _delay_ms(2000); // 2 seconds
-    
+    PORTE = 0x00;
     // open signal
-    PORTD = 0x40; // 0100 0000 => turning on the green LED
+    PORTH = 0x8; // 0000 1000 => turning on the green LED
     _delay_ms(5000); // 5 seconds
-    PORTD = 0x00; // 0000 0000 => turning off all LEDs
+    PORTH = 0x00; // 0000 0000 => turning off all LEDs
   }
 
   return 0;
